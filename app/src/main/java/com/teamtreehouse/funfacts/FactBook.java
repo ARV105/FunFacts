895128bb9ea3e81abec14090066aa6fa701e14ea
ARV <aavilaire@gmail.com>
2015-02-18 13:04:54 -0600

    changed facts to insect facts

--- app/src/main/java/com/teamtreehouse/funfacts/FactBook.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FactBook.java
@@ -11,17 +11,17 @@ public class FactBook {
     // Member variables (properties about the object)
    public String[] facts = {
 
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built.",
-            "Treehouse is not actually in a tree"
+            "Bed bug males stab females for insemination ",
+            "There are between 80-100 million insect species worldwide some still to be found.",
+            "There are 10 quintillion insects alive right now.",
+            "Aphids are asexual, female aphids reproduce parthenogenetically, they are clones.",
+            "Grylloblattid is an insect lives in ice caves and glaciers. They cannot survive in warm climates",
+            "Walking stick eggs resemble seeds so predators cannot seem them.",
+            "Ants are all female and males are produced only once a year for the mating flight",
+            "DNA cannot be extracted from ancient fossilized insects in amber",
+            "Entomophagy is the process of consuming insects for food",
+            "Most world crops are attacked by insects causing billions in damage",
+            "Insects will not take over the world, they already own it."
     };
 
 
